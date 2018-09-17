4c2c23c3278d2e9dbe6357b62046909b1e82b9b1
Mital B Topiwala <topiwal3@dh2026pc18.utm.utoronto.ca>
2018-09-17 11:54:00 -0400

    Adding CountTo10

---
+++ lab02/CountTo10.java
@@ -0,0 +1,24 @@
+public class CountTo10{
+
+    public static void main(String [] args){
+        for(int i = 1;i<= 10;i =i+1){
+            System.out.println(i);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+}//CountTo10
