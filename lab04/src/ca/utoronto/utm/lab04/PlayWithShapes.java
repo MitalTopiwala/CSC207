b7db21f0204bc4bfbae1c4d52fe6ec4fa5815a7f
Mital B Topiwala <topiwal3@dh2026pc14.utm.utoronto.ca>
2018-10-01 12:02:17 -0400

    completed PlayWithShapesClass

--- lab04/src/ca/utoronto/utm/lab04/PlayWithShapes.java
+++ lab04/src/ca/utoronto/utm/lab04/PlayWithShapes.java
@@ -1,20 +1,30 @@
 package ca.utoronto.utm.lab04;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PlayWithShapes {
 
 	// 0) Review OO notes/examples from lecture
 	// 1) Declare a variable "shapes" that will reference an array of reference to Shape
+	Shape [] shapes;
 	
 	public PlayWithShapes() {
 	
 		// 2) Make shapes equal a new array of 10 references to Shapes
-
+        shapes = new Shape[10];
 		
 		// 3) point shapes[0], ..., shapes[9] to new Rectangles and Circles 
-		shapes[0]=new Circle("red", 10, 5,5);
-		shapes[1]=new Circle("green", 20, 20, 15);
-		shapes[2]=new Rectangle("orange", 5, 10, 20, 40);
+		shapes[0]=new Circle("red", 10, 8,5);
+		shapes[1]=new Circle("green", 20, 28, 15);
+		shapes[2]=new Rectangle("orange", 5, 10, 20, 41);
+		shapes[3]=new Circle("red", 10, 5,5);
+		shapes[4]=new Circle("green", 20, 20, 15);
+		shapes[5]=new Rectangle("orange", 5, 17, 20, 10);
+		shapes[6]=new Circle("red", 10, 5,8);
+		shapes[7]=new Circle("blue", 20, 21, 16);
+		shapes[8]=new Rectangle("orange", 5, 18, 20, 44);
+		shapes[9]=new Circle("red", 10, 9,5);
+		
 		// ...
 		
 	}
@@ -40,9 +50,14 @@ public class PlayWithShapes {
 	 */
 	public void moveShapes(int dx, int dy) {
 		//TODO: Complete this method
-	}
+		
+		for(int i=0;i<10;i++) {
+		    shapes[i].setX(dx);
+		    shapes[i].setY(dy);
+		}//for
+	}//moveShapes
 
-	// 4) Complete colorShapes below to satisfy its javadoc
+	// 5) Complete colorShapes below to satisfy its javadoc
 
 	/**
 	 * Change the color of all referenced Shapes in the shapes array
@@ -51,14 +66,17 @@ public class PlayWithShapes {
 	 */
 	public void colorShapes(String c) {
 		//TODO: Complete this method
-	}
+		for(int i=0;i<10;i++) {
+		    shapes[i].setColor(c);
+		}//for
+	}//colorShapes
 	
 	
 	public static void main(String[] args) {
 		
 		PlayWithShapes s = new PlayWithShapes();
 		
-		// 5) Move all of the shapes around a bit
+		// 6) Move all of the shapes around a bit
 		Scanner scanner=new Scanner(System.in);
 		while(true){
 			System.out.println("Enter 'move' or 'color': ");
