9c2cd8eba92291c3cce71ebae0bf8c54d2166c2a
Mital B Topiwala <topiwal3@dh2020pc25.utm.utoronto.ca>
2018-10-01 17:41:22 -0400

    Finsihed sqaure class and adding comments to UnderstandingInheritance

--- lab04/src/ca/utoronto/utm/lab04/Square.java
+++ lab04/src/ca/utoronto/utm/lab04/Square.java
@@ -3,10 +3,10 @@ package ca.utoronto.utm.lab04;
 public class Square extends Rectangle{
 	
 	public Square() {
-		this("blue", 10, 10, 100, 100);
+		this("blue", 10, 100, 100);
 	}
 	public Square(String c, int width, int x, int y) {
-		Rectangle(c, width, width, x, y);
+		super(c, width, width, x, y);
 	}
 	
 
