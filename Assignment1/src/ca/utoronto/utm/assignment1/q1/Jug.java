ffdd69b409e6ef55c2e8b8035f5fad0593f0969d
Mital B Topiwala <topiwal3@dh2020pc25.utm.utoronto.ca>
2018-10-02 10:35:15 -0400

    Completed body of q1 methods

--- Assignment1/src/ca/utoronto/utm/assignment1/q1/Jug.java
+++ Assignment1/src/ca/utoronto/utm/assignment1/q1/Jug.java
@@ -2,16 +2,22 @@ package ca.utoronto.utm.assignment1.q1;
 
 public class Jug {
 	
-	public Jug() {
-		
-	}
+	int capacity;
+	int amount;
+	
+	public Jug(int capacity, int amount) {
+		this.capacity = capacity;
+		this.amount = amount;
+	}//constructor
+	
+	
 	
 	/**
 	 * Creates a string representation of Jug
 	 * @return A string representation of Jug
 	 **/
 	public String toString() {
-		String rV = "Return Value";
+		String rV = "("+this.amount+"/"+this.capacity+")";
 		return rV;
 	}//toString
 
--- Assignment1/src/ca/utoronto/utm/assignment1/q1/JugPuzzle.java
+++ Assignment1/src/ca/utoronto/utm/assignment1/q1/JugPuzzle.java
@@ -5,26 +5,46 @@ package ca.utoronto.utm.assignment1.q1;
  *
  */
 public class JugPuzzle {
+	private Jug[] jugList;
+	
+	private int numMoves;
 	
 	public JugPuzzle() {
+		jugList = new Jug[3];
+		this.jugList[0] = new Jug(8, 8);
+		this.jugList[1] = new Jug(5, 0);
+		this.jugList[2] = new Jug(3, 0);
+		
+		this.numMoves = 0;
 		
 	}//constructor
 	
 	/**
 	 *  Makes a move
-	 *  @param x the horizontal shift
-	 *  @param y the vertical shift
+	 *  @param from The jug water is spilled from
+	 *  @param to The jug water is being spilled into 
 	 **/
-	public static void move(int x, int y) {
+	public void move(int from, int to) {
+		int amountToPour = this.jugList[to].capacity - this.jugList[to].amount;
+		int amountInStart = this.jugList[from].amount;
+		
+		if (amountInStart >= amountToPour) {
+			this.jugList[from].amount = this.jugList[from].amount - amountToPour;
+			this.jugList[to].amount = this.jugList[to].amount + amountToPour;
+		}else {
+			this.jugList[from].amount = 0;
+			this.jugList[to].amount = this.jugList[to].amount + amountInStart;
+		}
+		this.numMoves += 1;
 		
 	}//move
 	
 	/**
-	 * Return the moves so far in JugPuzzle
+	 * Return the number of moves so far in JugPuzzle
 	 * @return Return the moves
 	 **/
-	public static int getMoves() {
-		return 1;
+	public int getMoves() {
+		return this.numMoves;
 	}//getmoves
 	
 	/**
@@ -32,7 +52,10 @@ public class JugPuzzle {
 	 * @return Return true iff the puzzle is solved
 	 **/
 	public boolean isPuzzleSolved() {
-		return true;
+		if(this.jugList[0].amount == 4 && this.jugList[1].amount ==4) {
+			return true;
+		}
+		return false;
 	}//isPuzzleSolved
 	
 	/**
@@ -40,7 +63,7 @@ public class JugPuzzle {
 	 * @return A string representation of JugPuzzle
 	 **/
     public String toString() {
-    	String rV = "Return Value";
+    	String rV = this.numMoves+ " 0:("+ this.jugList[0].amount+"/"+this.jugList[0].capacity+") 1:("+ this.jugList[1].amount+"/"+this.jugList[1].capacity+") 2:("+ this.jugList[2].amount+"/"+this.jugList[2].capacity+")";
 		return rV;
 	}//toString
 
