fdccd5096927f4ae3b906961c59ac8fcd8dcd41b
Mital B Topiwala <topiwal3@dh2020pc14.utm.utoronto.ca>
2018-10-03 19:13:12 -0400

    q1 version2

--- Assignment1/src/ca/utoronto/utm/assignment1/q1/Jug.java
+++ Assignment1/src/ca/utoronto/utm/assignment1/q1/Jug.java
@@ -5,6 +5,12 @@ public class Jug {
 	int capacity;
 	int amount;
 	
+	/**
+	 * Creates Jug 
+	 * @param capacity the amount the jug could hold
+	 * @param amount the amount of water in the cup
+	 * @return A string representation of Jug
+	 **/
 	public Jug(int capacity, int amount) {
 		this.capacity = capacity;
 		this.amount = amount;
