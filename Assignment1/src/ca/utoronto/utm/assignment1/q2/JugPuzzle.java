bfb9bf7b88a2f75155dea42ffbc3382ef02e3302
Mital B Topiwala <topiwal3@dh2020pc13.utm.utoronto.ca>
2018-10-04 18:25:54 -0400

    JugPuzzle and JugSolverController have been copied into q2, and have been modified to not refernce q1. notes.txt has also been filled

--- Assignment1/src/ca/utoronto/utm/assignment1/q2/JugPuzzle.java
+++ Assignment1/src/ca/utoronto/utm/assignment1/q2/JugPuzzle.java
@@ -1,30 +1,59 @@
 package ca.utoronto.utm.assignment1.q2;
+
 /**
  * 
  * @author csc207student 
  *
  */
 public class JugPuzzle {
+	private int [] jugAmount;
+	private int [] jugCapacity;
+	
+	private int numMoves;
 	
 	public JugPuzzle() {
+		jugAmount = new int[3];
+		jugCapacity = new int[3];
+		
+		this.jugCapacity[0] = 8;
+		this.jugCapacity[1] = 5;
+		this.jugCapacity[2] = 3;
+		
+		this.jugAmount[0] = 8;
+		this.jugAmount[1] = 0;
+		this.jugAmount[2] = 0;
+		
+		
+		this.numMoves = 0;
 		
 	}//constructor
 	
 	/**
 	 *  Makes a move
-	 *  @param x the horizontal shift
-	 *  @param y the vertical shift
+	 *  @param from The jug water is spilled from
+	 *  @param to The jug water is being spilled into 
 	 **/
-	public static void move(int x, int y) {
+	public void move(int from, int to) {
+		int amountToPour = this.jugCapacity[to] - this.jugAmount[to];
+		int amountInStart = this.jugAmount[from];
+		
+		if (amountInStart >= amountToPour) {
+			this.jugAmount[from] = this.jugAmount[from] - amountToPour;
+			this.jugAmount[to] = this.jugAmount[to] + amountToPour;
+		}else {
+			this.jugAmount[from] = 0;
+			this.jugAmount[to] = this.jugAmount[to] + amountInStart;
+		}
+		this.numMoves += 1;
 		
 	}//move
 	
 	/**
-	 * Return the moves so far in JugPuzzle
+	 * Return the number of moves taken so far in JugPuzzle
 	 * @return Return the moves
 	 **/
-	public static int getMoves() {
-		return 1;
+	public int getMoves() {
+		return this.numMoves;
 	}//getmoves
 	
 	/**
@@ -32,15 +61,18 @@ public class JugPuzzle {
 	 * @return Return true iff the puzzle is solved
 	 **/
 	public boolean isPuzzleSolved() {
-		return true;
+		if(this.jugAmount[0] == 4 && this.jugAmount[1] ==4) {
+			return true;
+		}
+		return false;
 	}//isPuzzleSolved
 	
 	/**
-	 * Creates a string representation of JugPuzzle
+	 * Creates and returns a string representation of JugPuzzle
 	 * @return A string representation of JugPuzzle
 	 **/
     public String toString() {
-    	String rV = "Return Value";
+    	String rV = this.numMoves+ " 0:("+ this.jugAmount[0]+"/"+this.jugCapacity[0]+") 1:("+ this.jugAmount[1]+"/"+this.jugCapacity[1]+") 2:("+ this.jugAmount[2]+"/"+this.jugCapacity[2]+")";
 		return rV;
 	}//toString
 
